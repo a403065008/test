fe2187c20cc4db19ae7c7d7f8d144f97708b91c2
a403065008 <94672679+a403065008@users.noreply.github.com>
2021-11-19 16:10:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+
+dog = Flask(__name__)
+
+
+@dog.route('/')
+def main():
+    return render_template('index.html')
+@dog.route('/home')
+def hello():
+    return render_template('test.html')
